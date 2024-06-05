858d2a6be5538faed9b32479d9dfd7f784cf1f0d
Mario Cesario <mario.henrique.cesario@gmail.com>
2024-06-05 14:42:07 -0300

    fazendo movimentos do peão

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,7 +17,7 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-	public int getMoveCouny() {
+	public int getMoveCount() {
 		return moveCount;
 	}
 	public void increaseMoveCount() {
